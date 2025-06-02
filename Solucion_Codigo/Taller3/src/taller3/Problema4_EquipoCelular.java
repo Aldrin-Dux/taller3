4a0c990c5fe8b6777ac823b62cec8a7798c03c5f
Aldrin Padilla Cairo <apadilla2@utpl.edu.ec>
2025-06-01 20:49:12 -0500

    Avanze

--- Solucion_Codigo/Taller3/src/taller3/Problema4_EquipoCelular.java
+++ Solucion_Codigo/Taller3/src/taller3/Problema4_EquipoCelular.java
@@ -1,13 +1,106 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taller3;
 
-/**
- *
- * @author aldrin
- */
 public class Problema4_EquipoCelular {
-    
+
+    public static void main(String[] args) {
+        EquipoCelular celularD = new EquipoCelular("Android", 9.00, 2350.80, 15.70, "I8:A5:88:8U:80:S3", "5698326489563258");
+        celularD.calcularCostoFinal();
+        System.out.println("Datos del dispositivo\n---------------------- " + celularD.toString());
+    }
+}
+
+class EquipoCelular {
+
+    public String sistemaOperativo;
+    public double tamañoPantalla;
+    public double costoInicial;
+    public double ivaPorcentaje;
+    public double ivaCosto;
+    public double costoFinal;
+    public String direccionMac;
+    public String direccionIMEI;
+
+    public EquipoCelular() {
+    }
+
+    public EquipoCelular(String sistemaOperativo, double tamañoPantalla, double costoInicial, double ivaPorcentaje, String direccionMac, String direccionIMEI) {
+        this.sistemaOperativo = sistemaOperativo;
+        this.tamañoPantalla = tamañoPantalla;
+        this.costoInicial = costoInicial;
+        this.ivaPorcentaje = ivaPorcentaje;
+        this.direccionMac = direccionMac;
+        this.direccionIMEI = direccionIMEI;
+    }
+
+    public void calcularCostoFinal() {
+        this.ivaCosto = this.costoInicial * (this.ivaPorcentaje / 100);
+        this.costoFinal = this.costoInicial + this.ivaCosto;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public void setTamañoPantalla(double tamañoPantalla) {
+        this.tamañoPantalla = tamañoPantalla;
+    }
+
+    public void setCostoIncial(double costoInicial) {
+        this.costoInicial = costoInicial;
+    }
+
+    public void setIvaPorcentaje(double ivaPorcentaje) {
+        this.ivaPorcentaje = ivaPorcentaje;
+    }
+
+    public void setDireccionMac(String direccionMac) {
+        this.direccionMac = direccionMac;
+    }
+
+    public void setDireccionIMEI(String direccionIMEI) {
+        this.direccionIMEI = direccionIMEI;
+    }
+
+    public String getSistemaOperativo() {
+        return this.sistemaOperativo;
+    }
+
+    public double getTamanioPantalla() {
+        return this.tamañoPantalla;
+    }
+
+    public double getCostoIncial() {
+        return this.costoInicial;
+    }
+
+    public double getIvaPorcentaje() {
+        return this.ivaPorcentaje;
+    }
+
+    public String getDireccionMac() {
+        return this.direccionMac;
+    }
+
+    public String getDireccionIMEI() {
+        return this.direccionIMEI;
+    }
+
+    public double getIvaCosto() {
+        return this.ivaCosto;
+    }
+
+    public double getCostoFinal() {
+        return this.costoFinal;
+    }
+
+    public String toString() {
+        return "\nSistema operativo: " + this.getSistemaOperativo()
+                + "\nTamanio pantalla: " + this.getTamanioPantalla()
+                + "\nCosto inicial: " + this.getCostoIncial()
+                + "\nIVA porcentaje: " + this.getIvaPorcentaje()
+                + "\nDireccion mac: " + this.getDireccionMac()
+                + "\nDireccion IMEI: " + this.getDireccionIMEI()
+                + "\nCosto del IVA inicial: " + this.getIvaCosto()
+                + "\nCosto Final: " + this.getCostoFinal();
+    }
 }
--- Solucion_Codigo/Taller3/src/taller3/Problema5_Estudiantes.java
+++ Solucion_Codigo/Taller3/src/taller3/Problema5_Estudiantes.java
@@ -1,13 +1,112 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taller3;
 
-/**
- *
- * @author aldrin
- */
+import java.util.Random;
+
 public class Problema5_Estudiantes {
-    
+
+    public static Estudiantes EstudianteD = new Estudiantes();
+
+    public static void main(String[] args) {
+        String[] nombres = {"Ana Garcia", "Luis Garcia", "Sofia Garcia", "Mateo Garcia", "Valentina Garcia", "Juliana Garcia", "Carlos Singh", "Daniela Kim"};
+
+        Random random = new Random();
+        for (int i = 0; i < nombres.length; i++) {
+            int indiceNombre = random.nextInt(nombres.length);
+            String nombreAleatorio = nombres[indiceNombre];
+            double calificacion_materia1 = random.nextDouble() * 7 + 1;
+            double calificacion_materia2 = random.nextDouble() * 5 + 1;
+            double calificacion_materia3 = random.nextDouble() * 6 + 1;
+            EstudianteD.setNombre(nombreAleatorio);
+            EstudianteD.setCalificacion1(calificacion_materia1);
+            EstudianteD.setCalificacion2(calificacion_materia2);
+            EstudianteD.setCalificacion3(calificacion_materia3);
+            EstudianteD.calcularPromedio();
+            EstudianteD.DeterminarEstado();
+            System.out.println("----------------------------------------------\nDatos del Estudiante " + i + ": " + EstudianteD.toString());
+        }
+
+    }
+}
+
+class Estudiantes {
+
+    public String nombre_estudiante;
+    double calificacion_materia1;
+    double calificacion_materia2;
+    double calificacion_materia3;
+    double promedio;
+    public String estado_estudiante;
+
+    public Estudiantes() {
+
+    }
+
+    public Estudiantes(String nombre_estudiante, String estado_estudiante, double calificacion_materia1, double calificacion_materia2, double calificacion_materia3) {
+        this.nombre_estudiante = nombre_estudiante;
+        this.calificacion_materia1 = calificacion_materia1;
+        this.calificacion_materia2 = calificacion_materia2;
+        this.calificacion_materia3 = calificacion_materia3;
+    }
+    public void calcularPromedio() {
+        this.promedio = (this.calificacion_materia1 + this.calificacion_materia2 + this.calificacion_materia3) / 3;
+    }
+
+    public void DeterminarEstado() {
+        if (this.promedio >= 6.5) {
+            this.estado_estudiante = "Aprobado";
+        } else {
+            this.estado_estudiante = "Reprobado";
+        }
+    }
+
+
+    public void setNombre(String nombre_estudiante) {
+        this.nombre_estudiante = nombre_estudiante;
+    }
+
+    public void setCalificacion1(double calificacion_materia1) {
+        this.calificacion_materia1 = calificacion_materia1;
+    }
+
+    public void setCalificacion2(double calificacion_materia2) {
+        this.calificacion_materia2 = calificacion_materia2;
+    }
+
+    public void setCalificacion3(double calificacion_materia3) {
+        this.calificacion_materia3 = calificacion_materia3;
+    }
+
+    public String getNombre() {
+        return this.nombre_estudiante;
+    }
+
+    public double getCalificacion1() {
+        return this.calificacion_materia1;
+    }
+
+    public double getCalificacion2() {
+        return this.calificacion_materia2;
+    }
+
+    public double getCalificacion3() {
+        return this.calificacion_materia3;
+    }
+
+    public double getPromedio() {
+        return this.promedio;
+    }
+
+    public String getEstablecerEstado() {
+        return this.estado_estudiante;
+    }
+
+
+    public String toString() {
+        return "\n\nNombre: " + this.getNombre()
+                + "\nCalificacion materia 1: " + this.getCalificacion1()
+                + "\nCalificacion materia 2: " + this.getCalificacion2()
+                + "\nCalificacion materia 3: " + this.getCalificacion3()
+                + "\nPromedio: " + this.getPromedio()
+                + "\nEstado del estudiante: " + this.getEstablecerEstado();
+    }
 }
